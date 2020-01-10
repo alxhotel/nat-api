a3f0e3adb584f178d15b37db50c69ea99fcf7afb
Alex <alxmorais8@msn.com>
2020-01-10 21:40:54 +0100

    Fix travis

--- tests/basic.js
+++ tests/basic.js
@@ -6,7 +6,7 @@ const port = 6690
 
 client.map({ publicPort: port, privatePort: port, protocol: 'UDP' }, function (err) {
   if (err) return console.log(err)
-  console.log('Port ' + port  + ' mapped to ' + port + ' (TCP)')
+  console.log('Port ' + port + ' mapped to ' + port + ' (TCP)')
 
   client.destroy(function () {
     console.log('NatAPI client destroyed')
